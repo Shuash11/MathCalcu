c838f1064065a3c63c0991c442b8b79ba9fd373d
Shuash11 <joashuabarimba10@gmail.com>
2026-04-11 17:40:09 +0800

    applying fixes

--- third_party/flutter_math_fork/lib/src/render/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_math_fork/src/ast/size.dart';
-
-const infiniteConstraint = BoxConstraints();
-
-const nullDelimiterSpace = Measurement(value: 0.12, unit: Unit.cssEm);
