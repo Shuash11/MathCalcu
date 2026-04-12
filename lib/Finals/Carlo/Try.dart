7963392c490bcbbe78739f90f40ed6b4467596e8
KarlsonDev <gamedeveloper745@gmail.com>
2026-04-12 16:06:21 +0800

    Try
    try

---
+++ lib/Finals/Carlo/Try.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tryan extends StatefulWidget {
+  const Tryan({super.key});
+
+  @override
+  State<Tryan> createState() => _TryanState();
+}
+
+class _TryanState extends State<Tryan> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
