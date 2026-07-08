ded0237c25e28da21e6ff00c0e385c521d127d09
Shuash11 <shuash11@users.noreply.github.com>
2026-07-08 10:19:19 +0800

    feat: add AppShell with bottom navigation bar

---
+++ lib/widgets/app_shell.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:calculus_system/screens/category_picker_screen.dart';
+import 'package:calculus_system/screens/settings_screen.dart';
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key});
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    CategoryPickerScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (index) {
+          setState(() => _currentIndex = index);
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            selectedIcon: Icon(Icons.settings_rounded),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
